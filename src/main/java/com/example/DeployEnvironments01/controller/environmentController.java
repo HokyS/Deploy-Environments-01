ca4cefaba68a9887d844502827cc049f1c45c937
Mario Cotumaccio <mariocotumaccio1@gmail.com>
2022-11-06 21:09:25 +0100

    Creating Environments Controller

---
+++ src/main/java/com/example/DeployEnvironments01/controller/environmentController.java
@@ -0,0 +1,23 @@
+package com.example.DeployEnvironments01.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class environmentController {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${devName.customVar}")
+    String devName;
+
+    @GetMapping("")
+    public String getAuthCode() {
+        return environment.getProperty("authCode.customVar") + devName;
+    }
+
+}
